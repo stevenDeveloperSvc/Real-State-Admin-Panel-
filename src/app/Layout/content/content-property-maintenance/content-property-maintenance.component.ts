7ab4a030b81b08f4d1ed2abcd2b2a304166e4ea1
stevenDeveloperSvc <IT-Development_2@lifestyleholidaysvc.com>
2024-04-17 11:17:39 -0400

    feat: marca de agua para las propiedades

--- src/app/Layout/content/content-property-maintenance/content-property-maintenance.component.ts
+++ src/app/Layout/content/content-property-maintenance/content-property-maintenance.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-content-property-maintenance',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ImageModule],
   templateUrl: './content-property-maintenance.component.html',
   styleUrl: './content-property-maintenance.component.scss'
 })
 export class ContentPropertyMaintenanceComponent {
 
+  selectedImageUrl?: string  | undefined ; 
+  display? : string | undefined ="SELECT AN IMAGE";
+  constructor() {}
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0]; 
+
+    if (file) {
+      const fileReader = new FileReader();
+      this.display = "";
+      fileReader.onload = (e: any) => {
+        this.selectedImageUrl = e.target.result;
+      };
+
+      fileReader.readAsDataURL(file);
+    }
+  }
+
+  DeleteCurrentImage() {
+      this.selectedImageUrl = undefined;
+      this.display ="SELECT AN IMAGE";
+
+    }
 }
