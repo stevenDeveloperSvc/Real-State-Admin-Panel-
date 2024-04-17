6cbd2b421844010f938aa08fb8051c330ee990ec
stevenDeveloperSvc <IT-Development_2@lifestyleholidaysvc.com>
2024-04-17 11:25:22 -0400

    feat: implementar marca de agua

--- src/app/Layout/content/content-property-maintenance/content-property-maintenance.component.ts
+++ src/app/Layout/content/content-property-maintenance/content-property-maintenance.component.ts
@@ -20,12 +20,32 @@ export class ContentPropertyMaintenanceComponent {
 
     if (file) {
       const fileReader = new FileReader();
-      this.display = "";
-      fileReader.onload = (e: any) => {
-        this.selectedImageUrl = e.target.result;
-      };
 
+      fileReader.onload = ( e : any) =>{
+        const img = new Image();
+        img.src = e.target.result;
+
+        img.onload = () =>{
+          const watermark = new Image();
+          watermark.src = 'assets/watermark.png'
+          watermark.onload = ()=>{
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+   
+            ctx?.drawImage(img, 0, 0);
+
+            const xPos = canvas.width - watermark.width -90
+            const yPos = canvas.height - watermark.height -90
+            ctx?.drawImage(watermark, xPos, yPos);
+            this.selectedImageUrl = canvas.toDataURL('image/png');
+          }
+        }
+      }
       fileReader.readAsDataURL(file);
+
     }
   }
 
